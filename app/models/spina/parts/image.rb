8f2c37e620edea3b8fe6474b02ab08435ae5b032
Ryan Romanchuk <ryan@romanch.uk>
2022-12-22 12:37:01 -0700

    uggghhhhhhhh

--- app/models/spina/parts/image.rb
+++ app/models/spina/parts/image.rb
@@ -1,7 +1,7 @@
 module Spina
   module Parts
     class Image < Base
-      attr_json :image_id, :integer, default: nil
+      attr_json :image_id, :string, default: nil
       attr_json :signed_blob_id, :string, default: nil
       attr_json :alt, :string, default: ""
       attr_json :filename, :string, default: ""
