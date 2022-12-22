733359c328091b248b7274b8229f1edaa4480fc3
Ryan Romanchuk <ryan@romanch.uk>
2022-12-22 12:39:14 -0700

    ugggh

--- app/models/spina/parts/attachment.rb
+++ app/models/spina/parts/attachment.rb
@@ -1,7 +1,7 @@
 module Spina
   module Parts
     class Attachment < Base
-      attr_json :attachment_id, :integer, default: nil
+      attr_json :attachment_id, :string, default: nil
       attr_json :signed_blob_id, :string, default: nil
       attr_json :filename, :string, default: ""
 
